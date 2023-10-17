68172fdb111d5261836b250c5d8c514d4393d0e9
BuildTools <unconfigured@null.spigotmc.org>
2023-10-17 18:59:30 +0800

    small fix, but on my pc

--- explodnt/game.js
+++ explodnt/game.js
@@ -1,14 +1,15 @@
-function setStyle( objId, propertyObject )
-{
- var elem = document.getElementById(objId);
- for (var property in propertyObject)
-    elem.style[property] = propertyObject[property];
+function setStyle(objId, propertyObject) {
+    var elem = document.getElementById(objId);
+    for (var property in propertyObject)
+        elem.style[property] = propertyObject[property];
 }
 
-console.log(document.getElementById("startText").style);
-
 function gameStart() {
-    setStyle("initButton",{'visibility':'hidden'});
-    setStyle("startText",{'animation':'fadeInAndOut 5s'});
-    setStyle("divBomb",{'animation':'fadeInn 3s'});
+    setStyle("initButton", { 'visibility': 'hidden' });
+    setStyle("startText", { 'animation': 'fadeInAndOut 5s ' });
+
+    setTimeout(function () {
+        setStyle("divBomb", { 'animation': 'fadeInn 5s', 'visibility': 'visible' });
+      }, 4000);
+
 }
